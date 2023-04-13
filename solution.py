f4b9bb737b9696722ce9b91257ddddf0a5ee9c1b
Replicantik <120032799+Replicantik@users.noreply.github.com>
2023-04-13 22:42:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 359238083 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.05
+    p_value=proportions_ztest([x_success, y_success], [x_cnt, y_cnt])[1] / 2 
+    if (p_value < alpha) and (x_success/x_cnt < y_success/y_cnt):
+      return True 
+    else: 
+      return False
